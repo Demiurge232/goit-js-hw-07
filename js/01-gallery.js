512dee82732d42ff9a4ff89680846cd2123c894f
Maksym Stankov <maksym.stankov@gmail.com>
2022-12-22 03:24:41 +0200

    task#1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const divGallery = document.querySelector('.gallery');
+const markup = galleryItems
+  .map(
+    ({ description, original, preview }) =>
+      `<div class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" ></a></div>`
+  )
+  .join('');
+divGallery.innerHTML = markup;
 
-console.log(galleryItems);
+const onDivGalleryClick = evt => {
+  evt.preventDefault();
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+  const instance = basicLightbox.create(`<img src="${evt.target.getAttribute('data-source')}">`);
+  instance.show();
+};
+
+divGallery.addEventListener('click', onDivGalleryClick);
